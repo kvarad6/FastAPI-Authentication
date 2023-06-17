90dc81bdc50cb1335fa666c5a190037ca21c5504
varad-kulkarni-searce <varad.kulkarni@searce.com>
2023-06-17 17:15:44 +0530

    updated

--- main.py
+++ main.py
@@ -2,10 +2,59 @@ import uvicorn
 from fastapi import FastAPI
 from app.model import PostSchema
 
-# Creating instance of fastapi class
+posts = [
+    {
+        "id": 1,
+        "title": "Penguins ",
+        "text": "Penguins are a group of aquatic flightless birds."
+    },
+    {
+        "id": 2,
+        "title": "Tigers ",
+        "text": "Tigers are the largest living cat species and a memeber of the genus panthera."
+    },
+    {
+        "id": 3,
+        "title": "Koalas ",
+        "text": "Koala is arboreal herbivorous maruspial native to Australia."
+    },
+]
+
+users = []
+
 app = FastAPI()
 
 
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
+
+
+# route handlers
+
+# testing
 @app.get("/", tags=["test"])
 def greet():
-    return {"Hello": "World"}
+    return {"hello": "world!."}
+
+
+# Get Posts
+@app.get("/posts", tags=["posts"])
+def get_posts():
+    return {"data": posts}
+
+
+@app.get("/posts/{id}", tags=["posts"])
+def get_single_post(id: int):
+    if id > len(posts):
+        return {
+            "error": "No such post with the supplied ID."
+        }
+
+    for post in posts:
+        if post["id"] == id:
+            return {
+                "data": post
+            }
