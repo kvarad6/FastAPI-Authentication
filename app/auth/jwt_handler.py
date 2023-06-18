4117e6186e6c7cc60522a27854e23ee4e681c308
varad-kulkarni-searce <varad.kulkarni@searce.com>
2023-06-18 13:19:39 +0530

    updated jwt_handler with encode n decode fun

---
+++ app/auth/jwt_handler.py
@@ -0,0 +1,40 @@
+# This file is responsible for signing , encoding , decoding and returning JWTS
+
+# Time is module will be responsible for setting an expiration limit of tokens
+import time
+
+# jwt module is responsibke for encoding and decoding generated token strings
+import jwt
+
+# decouple helps to organise settings so that you can change paramaters without having to redeploy application.
+# also makes it easy to store parameters in ini/.env file.
+from decouple import config
+
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+
+# function to return generated tokens (JWTs)
+def tokenResponse(token: str):
+    return {
+        "access token": token
+    }
+
+
+# function to sign the jwt strings
+def signJWT(userID: str):
+    payload = {
+        "userID": userID,
+        "expiry": time.time()+600
+    }
+    token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
+    return tokenResponse(token)
+
+
+# function to decode jwt
+def decodeJWT(token: str):
+    try:
+        decodeToken = jwt.decode(token, JWT_SECRET, JWT_ALGORITHM)
+        return decodeToken if decodeToken['expires'] > time.time() else None
+    except:
+        return {}
