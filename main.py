3df2fe2508ab566f1efa3ccd13a0f2a73741e0fe
varad-kulkarni-searce <varad.kulkarni@searce.com>
2023-06-18 19:52:11 +0530

    updated with userLogin, userSignup functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
-from fastapi import FastAPI
-from app.model import PostSchema
+from fastapi import FastAPI, Body
+from app.model import PostSchema, userSchema, userLoginSchema
+from app.auth.jwt_handler import signJWT
 
 posts = [
     {
@@ -42,12 +43,12 @@ def greet():
 
 # Get Posts
 @app.get("/posts", tags=["posts"])
-def get_posts():
+def getPosts():
     return {"data": posts}
 
 
 @app.get("/posts/{id}", tags=["posts"])
-def get_single_post(id: int):
+def getSinglePost(id: int):
     if id > len(posts):
         return {
             "error": "No such post with the supplied ID."
@@ -61,9 +62,36 @@ def get_single_post(id: int):
 
 
 @app.post("/posts", tags=["posts"])
-def add_post(post: PostSchema):
+def addPost(post: PostSchema):
     post.id = len(posts) + 1
     posts.append(post.dict())
     return {
         "data": "post added."
     }
+
+# route for user signup (create a new user)
+
+
+@app.post("/user/signup", tags=["user"])
+def userSignup(user: userSchema = Body(default=None)):
+    users.append(user)
+    return signJWT(user.email)
+
+
+def checkUser(data: userLoginSchema):
+    print("data:", data)
+    if user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+        else:
+            return False
+
+
+@app.post("/user/login", tags=["user"])
+def userLogin(user: userLoginSchema = Body(default=None)):
+    if checkUser(user):
+        return signJWT(user.email)
+    else:
+        return {
+            "Error": "Invalid login details"
+        }
